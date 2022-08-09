f24b97ff01f02c1bcc804efc1cf8172c4054edd4
rezakoushki95 <rezaaarhus@hotmail.com>
2022-08-09 17:46:58 +0200

    Finall app

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var messageLabel: UILabel!
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
-
-
+	@IBAction func eightSidedPressed(_ sender: UIButton) {
+		messageLabel.text = "You rolled a 8-sided dice and got a \(Int.random(in: 1...8))"
+	}
+	@IBAction func tenSidedPressed(_ sender: UIButton) {
+		messageLabel.text = "You rolled a 10-sided dice and got a \(Int.random(in: 1...10))"
+	}
+	@IBAction func twelveSidedPressed(_ sender: UIButton) {
+		messageLabel.text = "You rolled a 12-sided dice and got a \(Int.random(in: 1...12))"
+	}
+	
+	@IBAction func twentySidedPressed(_ sender: UIButton) {
+		messageLabel.text = "You rolled a 20-sided dice and got a \(Int.random(in: 1...20))"
+	}
+	@IBAction func hundredSidedPressed(_ sender: UIButton) {
+		messageLabel.text = "You rolled a 100-sided dice and got a \(Int.random(in: 1...100))"
+	}
+	
+	
+	@IBAction func fourSidedPressed(_ sender: UIButton) {
+		messageLabel.text = "You rolled a 4-sided dice and got a \(Int.random(in: 1...4))"
+	}
+	@IBAction func sixSidedPressed(_ sender: UIButton) {
+		messageLabel.text = "You rolled a 6-sided dice and got a \(Int.random(in: 1...6))"
+	}
 }
 
