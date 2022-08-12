e5b46a9225be788267073de3949fac3a1cd6d4de
rezakoushki95 <rezaaarhus@hotmail.com>
2022-08-12 21:43:10 +0200

    section 2.12 ex. 3 - modify app by connecting all buttons to a single connection

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -13,29 +13,29 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	}
-	@IBAction func eightSidedPressed(_ sender: UIButton) {
-		messageLabel.text = "You rolled a 8-sided dice and got a \(Int.random(in: 1...8))"
-	}
-	@IBAction func tenSidedPressed(_ sender: UIButton) {
-		messageLabel.text = "You rolled a 10-sided dice and got a \(Int.random(in: 1...10))"
-	}
-	@IBAction func twelveSidedPressed(_ sender: UIButton) {
-		messageLabel.text = "You rolled a 12-sided dice and got a \(Int.random(in: 1...12))"
-	}
-	
-	@IBAction func twentySidedPressed(_ sender: UIButton) {
-		messageLabel.text = "You rolled a 20-sided dice and got a \(Int.random(in: 1...20))"
-	}
-	@IBAction func hundredSidedPressed(_ sender: UIButton) {
-		messageLabel.text = "You rolled a 100-sided dice and got a \(Int.random(in: 1...100))"
-	}
-	
+//	@IBAction func eightSidedPressed(_ sender: UIButton) {
+//		messageLabel.text = "You rolled a 8-sided dice and got a \(Int.random(in: 1...8))"
+//	}
+//	@IBAction func tenSidedPressed(_ sender: UIButton) {
+//		messageLabel.text = "You rolled a 10-sided dice and got a \(Int.random(in: 1...10))"
+//	}
+//	@IBAction func twelveSidedPressed(_ sender: UIButton) {
+//		messageLabel.text = "You rolled a 12-sided dice and got a \(Int.random(in: 1...12))"
+//	}
+//
+//	@IBAction func twentySidedPressed(_ sender: UIButton) {
+//		messageLabel.text = "You rolled a 20-sided dice and got a \(Int.random(in: 1...20))"
+//	}
+//	@IBAction func hundredSidedPressed(_ sender: UIButton) {
+//		messageLabel.text = "You rolled a 100-sided dice and got a \(Int.random(in: 1...100))"
+//	}
+//
 	
 	@IBAction func fourSidedPressed(_ sender: UIButton) {
-		messageLabel.text = "You rolled a 4-sided dice and got a \(Int.random(in: 1...4))"
+		messageLabel.text = "You rolled a \(sender.titleLabel!.text!) dice and got a \(Int.random(in: 1...sender.tag))"
 	}
-	@IBAction func sixSidedPressed(_ sender: UIButton) {
-		messageLabel.text = "You rolled a 6-sided dice and got a \(Int.random(in: 1...6))"
-	}
+//	@IBAction func sixSidedPressed(_ sender: UIButton) {
+//		messageLabel.text = "You rolled a 6-sided dice and got a \(Int.random(in: 1...6))"
+//	}
 }
 
